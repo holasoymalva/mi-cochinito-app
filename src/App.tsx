18f8f38ce783fedd88f13caa9fa8884fe530641d
malvabombom <alquimista3301@gmail.com>
2022-01-09 20:17:46 -0600

    init react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mi cochinito</h1>
+    </div>
+  );
+}
+
+export default App;
